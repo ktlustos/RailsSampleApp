e130bf029ff04f424a7f6f1c2672528ff2214c57
Kent <ktlustos@arrayschool.com>
2017-01-25 14:18:25 -0700

    Finish up Users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name: "Kent Tlustos",
             email: "ktlustos@arrayschool.com",
             password: "password",
-            password_confirmation: "password")
+            password_confirmation: "password",
+            admin: true)
 
 99.times do |n|
   name = Faker::Name.name
