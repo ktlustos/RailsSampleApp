124561588b11b4c7a375cd41b8cfc172b22db4ad
Kent <ktlustos@arrayschool.com>
2017-02-06 14:48:40 -0700

    Update production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,13 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
+  host = "evening-sea-19576.herokuapp.com"
+  config.action_mailer.default_url_options = {host: host, protocol: 'https'}
+  config.action_mailer.perform_caching = false
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
